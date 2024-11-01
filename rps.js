e08f574a45ead7e7f92220c17c7441e5d50aa675
MathiasZ91git config --global user.email mzeibig@hotmail.deit commit -minitialgit commit -minitialgit commit -minitialgit config --global user.name MathiasZ91 <mzeibig@hotmail.de>
2024-11-01 15:39:53 +0100

    update2

--- rps.js
+++ rps.js
@@ -2,18 +2,21 @@ var input = require('readline-sync');
 var rpsInput = input.question('welcome to rock paper scissors. Please enter rock, paper, scissors or 0 to quit\no>> ');
 let rps = ['rock', 'paper', 'scissors'];
 
+// generates random number below 0 and 1 x when you multiply by 3  you get 0, 1, 2, 3
+//floor rounds it down to the nearest whole number
+
 while(userChoice!=0){
     let computerChoice = rps[Math.floor((Math.random() *3))];
     userChoice = userChoice.toLowerCase();
-
+// checks if the Computer wins 
     if((userChoicec =='rock'&&computerChoice =='paper') ||(userChoice =='paper'&&computerChoice =='scissors')||userChoice =='scissors'&&computerChoice =='rock'){
         console.log(`computer won. Computer choice: ${computerChoice}\n`);
         userChoice = input.question(`welcome to rock paper scissors. Please enter rock, paper, scissors or 0 to quit\no>> `)
-
+// checks if the user wins 
     }else if((userChoice =='paper'&&computerChoice =='rock')||(userChoice =='rock'&&computerChoice =='scissors')||(userChoice =='scissors'&&computerChoice == 'paper')){
         console.log(`You won. Computer choice: ${computerChoice}`);
         userChoice = input.question(`welcome to rock paper scissors. Please enter rock, paper, scissors or 0 to quit\n>> `);
-
+// checks if the its a tie 
     }else if(userChoice == computerChoice){
             console.log(`Its a tie. Computer Choice: ${computerChoice}`)
             userChoice = input.question(`welcome to rock paper scissors. Please enter rock, paper, scissors or 0 to quit\n>> `);
